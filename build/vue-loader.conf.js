8bcdbbead84cff5531d4b04d2b649d82d2eeb748
Steven Choo <stevenchoo.nl@gmail.com>
2018-08-07 08:30:12 +0200

    add: fix hot reload dev server

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -9,7 +9,7 @@ const sourceMapEnabled = isProduction
 module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
-    extract: !config.build.singlefile
+    extract: (isProduction && !config.build.singlefile)
   }),
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
