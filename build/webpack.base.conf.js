36178911b8522fe6b26c059b0f0df609db39c8e2
Ramon Rockx <ramonrockx@gmail.com>
2018-11-07 12:55:52 +0100

    change libraryTarget from umd to window

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -45,7 +45,7 @@ module.exports = {
     path: config.build.assetsRoot,
     publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
     filename: '[name].js',
-    libraryTarget: 'umd',
+    libraryTarget: 'window',
     // the name exported to window
     library: 'ConsentCookie',
   },
